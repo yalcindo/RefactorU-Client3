23e10464d5fcbe0b9ef8785435872d77f66d88b5
RU-006 <ru-006@Admins-Mac-mini-9.local>
2013-09-24 08:54:41 -0600

    Comments Removed

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-//***Experimentation trying to use loops
+// ***Experimentation trying to use loops
 var questions=[" Enter Your Phone Number with Dashes:","Birth Date format xx/xx/xx:","Postal Code:","State Abbrv:","Married"];
 
 
@@ -6,143 +6,138 @@ for(var i=0;i<questions.length;i++)
 {
    
    // --------------Phone number info------------------------
-   // if (i===0)
-   // {
-   // 	    var answer=prompt(questions[i]);
-   //       var correctAn= answer.charAt(3)=== "-" && answer.charAt(7) === "-";
+   if (i===0)
+   {
+   	    var answer=prompt(questions[i]);
+         var correctAn= answer.charAt(3)=== "-" && answer.charAt(7) === "-";
 
-   //      if(correctAn)
-   //     {
-   //   	alert("True");
-   //     }
-   //       else
-   //       {
-   //   	   while(!correctAn)
-   //   	    {
-   //   		alert("False");
-   //   	    var answer=prompt(questions[i]);
-   //       	var correctAn= answer.charAt(3)=== "-" && answer.charAt(7) === "-";
+        if(correctAn)
+        {
+     	   alert("True");
+        }
+        else
+        {
+     	    while(!correctAn)
+     	    {
+         		  alert("False");
+         	    var answer=prompt(questions[i]);
+             	var correctAn= answer.charAt(3)=== "-" && answer.charAt(7) === "-";
 
-   //        		if(correctAn)
-   //            		{
-   //   	      			alert("True");
-   //           		}
-   //      }
-   //         }
-   // }
+          		if(correctAn)
+              	{
+     	      			alert("True");
+             		}
+          }
+        }
+    }
 
- //-------------Birth date----------------------------
-   //  if (i===1)
-   // {
-   // 	    var answer=prompt(questions[i]);
-   //       var correctAn= answer.charAt(2)=== "/" && answer.charAt(5) === "/";
+ // -------------Birth date----------------------------
+    if (i===1)
+   {
+   	    var answer=prompt(questions[i]);
+        var correctAn= answer.charAt(2)=== "/" && answer.charAt(5) === "/";
 
-   //      if(correctAn)
-   //     {
-   //   	alert("True");
-   //     }
-   //       else
-   //       {
-   //   	   while(!correctAn)
-   //   	    {
-   //   		alert("False");
-   //   	    var answer=prompt(questions[i]);
-   //       	var correctAn= answer.charAt(2)=== "/" && answer.charAt(5) === "/";
+        if(correctAn)
+        {
+     	    alert("True");
+        }
+         else
+         {
+     	     while(!correctAn)
+     	     {
+         		  alert("False");
+         	    var answer=prompt(questions[i]);
+             	var correctAn= answer.charAt(2)=== "/" && answer.charAt(5) === "/";
 
-   //        		if(correctAn)
-   //            		{
-   //   	      			alert("True");
-   //           		}
-   //      }
-   //         }
-   // }
+          		if(correctAn)
+              {
+     	      		alert("True");
+             	}
+            }
+          }
+    }
 
-//--------------------Postal Code------------
+// --------------------Postal Code------------
 
-// if (i===2)
-//    {
-//    	    var answer=prompt(questions[i]);
-//          var correctAn= answer.length===5 || (answer.length ===10 && answer.charAt(5)==="-");
+if (i===2)
+   {
+   	    var answer=prompt(questions[i]);
+        var correctAn= answer.length===5 || (answer.length ===10 && answer.charAt(5)==="-");
 
-//         if(correctAn)
-//        {
-//      	alert("True");
-//        }
-//          else
-//          {
-//      	   while(!correctAn)
-//      	    {
-//      		alert("False");
-//      	    var answer=prompt(questions[i]);
-//          	var correctAn= answer.length===5 || (answer.length ===10 && answer.charAt(5)==="-");
+        if(correctAn)
+       {
+     	    alert("True");
+       }
+         else
+         {
+       	     while(!correctAn)
+       	    {
+       		     alert("False");
+       	       var answer=prompt(questions[i]);
+           	   var correctAn= answer.length===5 || (answer.length ===10 && answer.charAt(5)==="-");
 
-//           		if(correctAn)
-//               		{
-//      	      			alert("True");
-//              		}
-//         }
-//            }
-//    }
+            		if(correctAn)
+                {
+       	      	 alert("True");
+               	}
+            }
+          }
+    }
 
-   //-------------------- State Abbrevetion ------------
+   // -------------------- State Abbrevetion ------------
 
-   // if (i===3)
-   // {
-   // 	    var answer=prompt(questions[i]);
-   //       var correctAn= answer.length===2 && answer === answer.toUpperCase();
+   if (i===3)
+   {
+   	      var answer=prompt(questions[i]);
+          var correctAn= answer.length===2 && answer === answer.toUpperCase();
 
-   //      if(correctAn)
-   //     {
-   //   	alert("True");
-   //     }
-   //       else
-   //       {
-   //   	   while(!correctAn)
-   //   	    {
-   //   		alert("False");
-   //   	    var answer=prompt(questions[i]);
-   //       	var correctAn= answer.length===2 && answer === answer.toUpperCase();
+        if(correctAn)
+        {
+     	    alert("True");
+        }  
+        else
+        {
+     	     while(!correctAn)
+     	     {
+     		     alert("False");
+     	       var answer=prompt(questions[i]);
+         	   var correctAn= answer.length===2 && answer === answer.toUpperCase();
 
-   //        		if(correctAn)
-   //            		{
-   //   	      			alert("True");
-   //           		}
-   //      }
-   //         }
-   // }
+          		if(correctAn)
+              {
+     	      			 alert("True");
+             	}
+            }
+        }
+   }
 
      //-------------------- MArried ------------
    if (i===4)
    {
-   	    var answer=prompt(questions[i]);
+   	     var answer=prompt(questions[i]);
          var correctAn= answer.toLowerCase()==="yes" || answer.toUpperCase() ==="no";
 
         if(correctAn)
-       {
-     	alert("Cool!");
-       }
+        {
+     	    alert("Cool!");
+         }
          else
          {
-     	   while(!correctAn)
-     	    {
-     		alert("Enter yes or No");
-     	    var answer=prompt(questions[i]);
-         	var correctAn= answer.toLowerCase()==="yes" || answer.toUpperCase() ==="no";
+     	      while(!correctAn)
+     	      {
+     		    alert("Enter yes or No");
+     	      var answer=prompt(questions[i]);
+         	  var correctAn= answer.toLowerCase()==="yes" || answer.toUpperCase() ==="no";
 
           		if(correctAn)
-              		{
-     	      			alert("Cool!");
-             		}
-        }
-           }
-   }
+              {
+     	      		alert("Cool!");
+             	}
+            }
+          }
+    }
          
- // ---------------------------Playing around-----------------------------
-
-
-
-
-
+ // ---------------------------Playing around----------------------------
 
 }
 
